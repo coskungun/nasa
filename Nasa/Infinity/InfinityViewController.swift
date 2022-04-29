be0e1d14ce71c0355acd62cc0a8ce9da6c887890
Coşkun <coskungun@gmail.com>
2022-04-29 20:45:11 +0300

    Inf. has been completed.
    Inf. has been completed.

--- Nasa/Infinity/InfinityViewController.swift
+++ Nasa/Infinity/InfinityViewController.swift
@@ -18,6 +18,7 @@ final class CVCell: UICollectionViewCell {
         super.init(frame: frame)
         addSubview(imageView)
         imageView.frame = CGRect.init(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
+        imageView.backgroundColor = .clear
         imageView.contentMode = .scaleAspectFit
         
     }
@@ -47,8 +48,8 @@ class InfinityViewController: UIViewController,InfinityViewModelDelegate {
         super.viewWillAppear(animated)
         collectionView.frame = view.bounds
         
-        pageControl.frame.origin.y = view.bounds.maxY - 80 - pageControl.frame.height
-        pageControl.sizeToFit()
+        pageControl.frame = CGRect.init(x: (self.view.frame.size.width / 2) - 125, y: self.view.frame.size.height - 100, width: 250, height: 60)
+        self.view.bringSubviewToFront(pageControl)
     }
     
     func handleViewModelOutput(_ output: InfinityModelViewModelOutput) {
@@ -58,27 +59,34 @@ class InfinityViewController: UIViewController,InfinityViewModelDelegate {
         case .reloadData:
             collectionView.alpha = 1
             collectionView.reloadData()
+            pageControl.numberOfPages = viewModel.arrData?.count ?? 0
+            pageControl.alpha = 1
         case .updateTitle(let title):
             self.title = title
         }
     }
     
     func setupUI(){
+        pageControl.alpha = 0
         collectionView.alpha = 0
         view.addSubview(collectionView)
         view.addSubview(pageControl)
-        pageControl.numberOfPages = viewModel.arrData?.count ?? 0
+        pageControl.numberOfPages = 10
         pageControl.tintColor = .black
-        pageControl.backgroundColor = .red
         
         collectionView.register(CVCell.self, forCellWithReuseIdentifier:"id")
         collectionView.reloadData()
+        collectionView.backgroundColor = .clear
         collectionView.carouselDataSource = self
         collectionView.isAutoscrollEnabled = true
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.autoscrollTimeInterval = 3.0
         let size = UIScreen.main.bounds.size
         collectionView.flowLayout.itemSize = CGSize(width: size.width, height: size.height)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("ellendi")
     }
 }
 
@@ -93,7 +101,6 @@ extension InfinityViewController: CarouselCollectionViewDataSource {
         let url = URL(string: viewModel.arrData?[index].img_src ?? "")
         cell.imageView.kf.setImage(with: url)
         
-//        cell.imageView.kf.setImage(with: viewModel.arrData?[index].img_src ?? "", placeholder: UIImage.init(named: "profile_avatar_1"),completionHandler: {})
         return cell
     }
     
